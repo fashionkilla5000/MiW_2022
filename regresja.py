de3dc799c60354e3ae76ea91b28a1eccb59754c7
FashionKilla5000 <kwascraft@gmail.com>
2022-06-09 10:46:12 +0200

    poprawki

--- regresja.py
+++ regresja.py
@@ -1,9 +1,10 @@
 import random as r
 import numpy as np
+from numpy.linalg import inv
 
 x = np.array([0, 1, 2, 3, 4, 5])
-X = np.array([[2, 5, 7, 8], [1, 1, 1, 1]])
-Y = np.array([1, 2, 3, 3])
+
+y = np.array([1, 2, 3, 3, 4, 5])
 
 
 def srednia(x):
@@ -20,13 +21,20 @@ def odchylenie(x):
     return s
 
 
-# print(srednia(x))
-# print(odchylenie(x))
+print(srednia(x))
+print(odchylenie(x))
 
-def regresja(X, Y):
-    x = np.append(X, [1, 1, 1, 1])
-    print(x)
-    # xTx =
+def regresja(x, y):
+
+    x = np.concatenate(([np.ones(len(x))],[x]),axis=0).transpose()
+
+    xTx = np.dot(x.transpose(),x)
+    xTy = np.dot(x.transpose(),y)
+
+    B = np.dot(inv(xTx),xTy)
+
+    print('y=',B[0],'+',B[1],'x')
 
 
-print(regresja(X, Y))
+
+print(regresja(x, y))
